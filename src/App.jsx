da1f2ab7ad703c1d599ce09b51df569917427085
Maneesh Cheema <maneeshcheema17@gmail.com>
2022-12-11 02:58:37 +0530

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Starred from "./pages/Starred";
 function App() {
   return (
     <div>
       <Nav></Nav>
       <Switch>
         <Route exact path="/">
-          This is home page
+          <Home />
         </Route>
         <Route exact path="/starred">
-          This is starred
+          <Starred />
         </Route>
-        <Route>this is 404 page</Route>
+        <Route>
+          <div>Page Not Found</div>
+        </Route>
       </Switch>
     </div>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const LINKS = [
+  { to: "/", text: "Home" },
+  { to: "/starred", text: "Starred" },
+];
+
 const Nav = () => {
   return (
     <div>
       <ul>
-        <li>
-          <Link to="/starred">Go to starred page</Link>
-        </li>
+        {LINKS.map((item) => (
+          <li key={item.to}>
+            <Link to={item.to}>{item.text}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
